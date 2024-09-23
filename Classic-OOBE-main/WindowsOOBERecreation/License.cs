f92f122ea4ea88f8a6790d8e1a6d4ba817651d7e
neptuneen <144282272+catneptune@users.noreply.github.com>
2024-09-23 18:26:16 -0300

    Add files via upload

---
+++ Classic-OOBE-main/WindowsOOBERecreation/License.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsOOBERecreation
+{
+    public partial class License : Form
+    {
+        private Main _mainForm;
+
+        public License(Main mainForm)
+        {
+            InitializeComponent();
+            _mainForm = mainForm;
+
+            nextButton.Enabled = false;
+            LoadLicenseFile();
+            checkBox1.CheckedChanged += CheckBox1_CheckedChanged;
+        }
+
+        private void LoadLicenseFile()
+        {
+            string licensePath = @"C:\Windows\System32\license.rtf";
+
+            try
+            {
+                if (File.Exists(licensePath))
+                {
+                    richTextBox1.LoadFile(licensePath);
+                }
+                else
+                {
+                    MessageBox.Show("License file not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while loading the license: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            nextButton.Enabled = checkBox1.Checked;
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            Finalizing finalizingForm = new Finalizing(_mainForm);
+            _mainForm.LoadFormIntoPanel(finalizingForm);
+        }
+    }
+}
---
+++ Classic-OOBE-main/WindowsOOBERecreation/Start.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsOOBERecreation
+{
+    public partial class Start : Form
+    {
+        private Main _mainForm;
+
+        public string Username { get; private set; }
+        public string ComputerName { get; private set; }
+
+        public Start(Main mainForm)
+        {
+            InitializeComponent();
+            _mainForm = mainForm;
+
+            usernameBox.TextChanged += UsernameBox_TextChanged;
+            usernameBox.AutoSize = false;
+            usernameBox.Height = 20;
+            computernameBox.KeyPress += ComputernameBox_KeyPress;
+            computernameBox.AutoSize = false;
+            computernameBox.Height = 20;
+            nextButton.Enabled = false;
+        }
+
+        private void UsernameBox_TextChanged(object sender, EventArgs e)
+        {
+            string sanitizedUsername = usernameBox.Text.Replace(" ", string.Empty);
+            computernameBox.Text = $"{sanitizedUsername}-PC";
+
+            nextButton.Enabled = !string.IsNullOrWhiteSpace(usernameBox.Text);
+
+            Username = sanitizedUsername;
+            ComputerName = computernameBox.Text;
+        }
+
+        private void ComputernameBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ' ')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            _mainForm.Username = Username;
+            _mainForm.ComputerName = ComputerName;
+
+            _mainForm.LoadPasswordForm();
+        }
+    }
+}
